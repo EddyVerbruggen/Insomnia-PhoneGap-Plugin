c1f46d89a56bfa3ab2ddf7bbb46b5914f53dba93
Jesse MacFadyen <purplecabbage@gmail.com>
2014-05-30 17:49:55 -0700

    cleanup and add correct call for allowSleepAgain

--- src/wp8/Insomnia.cs
+++ src/wp8/Insomnia.cs
@@ -1,24 +1,9 @@
-
-using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Info;
-using System.IO.IsolatedStorage;
-using System.Windows.Resources;
-
+using Microsoft.Phone.Shell;
 
 namespace WPCordovaClassLib.Cordova.Commands
 {
     public class Insomnia : BaseCommand
     {
-
         public void keepAwake(string options) 
         {
             PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
@@ -27,7 +12,7 @@ namespace WPCordovaClassLib.Cordova.Commands
 
         public void allowSleepAgain(string options) 
         {
-            PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled; //??
+            PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Enabled;
             DispatchCommandResult(new PluginResult(PluginResult.Status.OK));
         }
     }
